29cd645dfa69828ba198340e1a63df4b53115484
sturmgans <fredrik@danebjer.com>
2020-06-22 19:29:18 +0200

    Added FreeRTOS implementations, taken from the espressif demos in FreeRTOS.

--- src/freertos/fsu_eye_freertos_callbacks.c
+++ src/freertos/fsu_eye_freertos_callbacks.c
@@ -26,25 +26,58 @@
 
 void vApplicationGetIdleTaskMemory(void)
 {
- // TODO: Implement
+  /* If the buffers to be provided to the Idle task are declared inside this
+   * function then they must be declared static - otherwise they will be allocated on
+   * the stack and so not exists after this function exits. */
+  static StaticTask_t xIdleTaskTCB;
+  static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+
+  /* Pass out a pointer to the StaticTask_t structure in which the Idle
+   * task's state will be stored. */
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+
+  /* Pass out the array that will be used as the Idle task's stack. */
+  *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+
+  /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
+   * Note that, as the array is necessarily of type StackType_t,
+   * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
 }
 
+extern void esp_vApplicationIdleHook();
 void vApplicationIdleHook(void)
 {
- // TODO: Implement
+  esp_vApplicationIdleHook();
 }
 
-void vApplicationTickHook(void)
+extern void esp_vApplicationTickHook();
+void IRAM_ATTR vApplicationTickHook()
 {
- // TODO: Implement
+    esp_vApplicationTickHook();
 }
 
 void vApplicationGetTimerTaskMemory(void)
 {
- // TODO: Implement
+  /* If the buffers to be provided to the Timer task are declared inside this
+   * function then they must be declared static - otherwise they will be allocated on
+   * the stack and so not exists after this function exits. */
+  static StaticTask_t xTimerTaskTCB;
+  static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
+
+  /* Pass out a pointer to the StaticTask_t structure in which the Idle
+   * task's state will be stored. */
+  *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+
+  /* Pass out the array that will be used as the Timer task's stack. */
+  *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+
+  /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+   * Note that, as the array is necessarily of type StackType_t,
+   * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+  *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
 }
 
 void vApplicationDaemonTaskStartupHook(void)
 {
- // TODO: Implement
 }
