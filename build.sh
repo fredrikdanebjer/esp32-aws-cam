5c10ce5e526dd1e96d6b6b613a20b4919c7cd80d
sturmgans <fredrik@danebjer.com>
2020-06-29 19:35:44 +0200

    The sdkconfig.defaults line was incorrect in the buildscript, that has been fixed

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ elif [[ $# -eq 2 ]] && [[ $1 = "debug" ]] && [[ "${2}" =~ ^[0-9]+$ ]]; then
   echo Setting DEBUG_PRINT_LEVEL to $2
   rm -rf build
   mkdir build
-  cmake -S . -B build -D_DEBUG_PRINT_LEVEL=$2 -DISF_SDK_CONFIG_DEFAULTS=config/esp32/sdkconfig.defaults -DCMAKE_TOOLCHAIN_FILE=external/freertos/tools/cmake/toolchains/xtensa-esp32.cmake -G Ninja
+  cmake -S . -B build -D_DEBUG_PRINT_LEVEL=$2 -DIDF_SDK_CONFIG_DEFAULTS=config/esp32/sdkconfig.defaults -DCMAKE_TOOLCHAIN_FILE=external/freertos/tools/cmake/toolchains/xtensa-esp32.cmake -G Ninja
   cmake --build build
 elif [[ $# -eq 1 ]] && [[ $1 = "flash" ]]; then
   cmake --build build --target flash
@@ -30,7 +30,7 @@ elif [[ $# -eq 1 ]] && [[ $1 = "monitor" ]]; then
 elif [[ $# -eq 1 ]] && [[ $1 = "all" ]]; then
   rm -rf build
   mkdir build
-  cmake -S . -B build -D_DEBUG_PRINT_LEVEL=9 -DISF_SDK_CONFIG_DEFAULTS=config/esp32/sdkconfig.defaults -DCMAKE_TOOLCHAIN_FILE=external/freertos/tools/cmake/toolchains/xtensa-esp32.cmake -G Ninja
+  cmake -S . -B build -D_DEBUG_PRINT_LEVEL=9 -DIDF_SDK_CONFIG_DEFAULTS=config/esp32/sdkconfig.defaults -DCMAKE_TOOLCHAIN_FILE=external/freertos/tools/cmake/toolchains/xtensa-esp32.cmake -G Ninja
   cmake --build build --target flash
   ./external/freertos/vendors/espressif/esp-idf/tools/idf.py monitor -p ${MONITOR_DEVICE} -B build
 else
