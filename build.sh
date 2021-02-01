f25eddde6c2f124537945de0dd5932f5a26ab63f
sturmgans <fredrik@danebjer.com>
2021-02-01 16:01:29 +0100

    Made UART selection an argument for relevant build commands.

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-MONITOR_DEVICE=/dev/ttyUSB0
-
 BUILD_DIR="$(pwd)/build"
 COMPILER_DIR="$(pwd)/external/esp32-toolchain/xtensa-esp32-elf/bin/"
 
@@ -26,21 +24,21 @@ elif [[ $# -eq 1 ]] && [[ $1 = "flash" ]]; then
   cmake --build build --target flash
 elif [[ $# -eq 1 ]] && [[ $1 = "erase" ]]; then
   ./external/freertos/vendors/espressif/esp-idf/tools/idf.py erase_flash
-elif [[ $# -eq 1 ]] && [[ $1 = "monitor" ]]; then
-  echo "Trying to monitor with device ${MONITOR_DEVICE}. If your connection is using another device, please change in script"
-  ./external/freertos/vendors/espressif/esp-idf/tools/idf.py monitor -p ${MONITOR_DEVICE} -B build
-elif [[ $# -eq 1 ]] && [[ $1 = "all" ]]; then
+elif [[ $# -eq 2 ]] && [[ $1 = "monitor" ]]; then
+  echo "Trying to monitor with device $2. If your connection is using another device, please change in script"
+  ./external/freertos/vendors/espressif/esp-idf/tools/idf.py monitor -p $2 -B build
+elif [[ $# -eq 2 ]] && [[ $1 = "all" ]]; then
   rm -rf build
   mkdir build
   cmake -S . -B build -D_DEBUG_PRINT_LEVEL=9 -DIDF_SDK_CONFIG_DEFAULTS=config/esp32/sdkconfig.defaults -DCMAKE_TOOLCHAIN_FILE=external/freertos/tools/cmake/toolchains/xtensa-esp32.cmake -G Ninja
   cmake --build build --target flash
-  ./external/freertos/vendors/espressif/esp-idf/tools/idf.py monitor -p ${MONITOR_DEVICE} -B build
+  ./external/freertos/vendors/espressif/esp-idf/tools/idf.py monitor -p $2 -B build
 else
   echo "Unknown command! Usage:"
   echo "./build.sh                - compile"
   echo "./build.sh debug <level>  - compile with debug logs enabled at provided level"
   echo "./build.sh flash          - flash"
-  echo "./build.sh monitor        - monitor output"
+  echo "./build.sh monitor <port> - monitor output"
   echo "./build.sh erase          - erase flash"
-  echo "./build.sh all            - builds with max highest debug resolution, flashes and starts monitoring"
+  echo "./build.sh all <port>     - builds with max highest debug resolution, flashes and starts monitoring"
 fi
